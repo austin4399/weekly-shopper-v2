ae0e0c78c48167021f19dc28471ad91c1fd892c2
auhallet <austin.hallett-doughty@silabs.com>
2022-07-10 16:58:49 -0400

    added mui theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppRouter } from "../server/router";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@mui/material";
+import theme from '../styles/themes';
 import "../styles/globals.css";
 
 const MyApp: AppType = ({
@@ -12,7 +14,9 @@ const MyApp: AppType = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 };
